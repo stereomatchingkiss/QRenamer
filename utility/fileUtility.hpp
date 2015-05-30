66bf250f5d8ae7a93dcceac33e3e97131bdfaa50
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-05-30 21:56:25 +0800

    remove useless files

--- utility/fileUtility.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FILEUTILITY_HPP
-#define FILEUTILITY_HPP
-
-#include <QStringList>
-
-void rename_files(QString const &dir, QStringList const &old_file_names,
-                  QStringList const &new_file_names);
-
-#endif // FILEUTILITY_HPP
